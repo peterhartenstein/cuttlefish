8444deadb49e317ae038a82d8ee57ff34ac8be93
Peter Hartenstein <contact@peterhartenstein.com>
2024-09-25 09:54:12 +0200

    bump version

--- Blender Plugin/cuttlefish.py
+++ Blender Plugin/cuttlefish.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "cuttlefish",
     "author": "Peter Hartenstein",
-    "version": (0, 0, 1),
+    "version": (0, 0, 2),
     "blender": (4, 20, 0),
     "location": "3D Viewport > Sidebar > cuttlefish",
     "description": "Translating animated geometry from Blender to Grasshopper",
